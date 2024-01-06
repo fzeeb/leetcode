e92cc22293837c42ec918bc910efb63adc22573e
Felix <felix.zeeb@pm.me>
2024-01-06 15:37:42 +0100

    Make more efficent with binary search instead of nested loop

--- jobScheduling.js
+++ jobScheduling.js
@@ -12,35 +12,41 @@ If you choose a job that ends at time X you will be able to start another job th
  * @return {number}
  */
 var jobScheduling = function(startTime, endTime, profit) {
-  // create an array to store the jobs
-  const jobs = [];
-  // loop through the startTime array
-  for (let i = 0; i < startTime.length; i++) {
-    // push the startTime, endTime, and profit into the jobs array
-    jobs.push([startTime[i], endTime[i], profit[i]]);
-  }
-  // sort the jobs array by the endTime
+  const jobs = startTime.map((start, index) => [start, endTime[index], profit[index]]);
   jobs.sort((a, b) => a[1] - b[1]);
-  // create an array to store the profit
-  const dp = new Array(startTime.length).fill(0);
+
+  const dp = Array(jobs.length).fill(0);
   dp[0] = jobs[0][2];
-  // loop through the jobs array
-  for (let i = 1; i < jobs.length; i++) {
-    // create a variable to store the current profit
-    let currProfit = jobs[i][2];
-    // loop through the jobs array
-    for (let j = i - 1; j >= 0; j--) {
-      // if the endTime of the current job is greater than the startTime of the previous job
-      if (jobs[i][0] >= jobs[j][1]) {
-        // update the current profit
-        currProfit = Math.max(currProfit, jobs[i][2] + dp[j]);
+
+  const binarySearch = (i) => {
+    let start = 0;
+    let end = i - 1;
+    while (start <= end) {
+      let mid = Math.floor((start + end) / 2);
+      if (jobs[mid][1] <= jobs[i][0]) {
+        if (jobs[mid + 1][1] <= jobs[i][0]) {
+          start = mid + 1;
+        } else {
+          return mid;
+        }
+      } else {
+        end = mid - 1;
       }
     }
-    // update the dp array
+    return -1;
+  };
+
+  for (let i = 1; i < jobs.length; i++) {
+    let currProfit = jobs[i][2];
+    let l = binarySearch(i);
+    if (l != -1) {
+      currProfit += dp[l];
+    }
     dp[i] = Math.max(currProfit, dp[i - 1]);
   }
+
   return dp[dp.length - 1];
-};  
+} 
 
 const startTime = [1,2,3,3], endTime = [3,4,5,6], profit = [50,10,40,70]
 console.log(jobScheduling(startTime, endTime, profit)) // 120
