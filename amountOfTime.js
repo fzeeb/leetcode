7edf278b748bc89b41a9a75ab08b791326bc9b60
Felix <felix.zeeb@pm.me>
2024-01-10 16:16:49 +0100

    breadth-first search

--- amountOfTime.js
+++ amountOfTime.js
@@ -11,9 +11,50 @@
  * @param {number} start
  * @return {number}
  */
+
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+  }
+
 var amountOfTime = function(root, start) {
-    
+  const graph = new Map();
+  const queue = [[start, 0]];
+  const infected = new Set();
+
+  const buildGraph = (node, parent) => {
+    if (node === null) return;
+    if (!graph.has(node.val)) graph.set(node.val, []);
+    if (parent !== null) {
+      graph.get(node.val).push(parent.val);
+      graph.get(parent.val).push(node.val);
+    }
+    buildGraph(node.left, node);
+    buildGraph(node.right, node);
+  };
+
+  buildGraph(root, null);
+
+  let minutes = 0;
+  while (queue.length > 0) {
+    const [node, time] = queue.shift();
+    if (!infected.has(node)) {
+      infected.add(node);
+      minutes = Math.max(minutes, time);
+      for (const neighbor of graph.get(node)) {
+        queue.push([neighbor, time + 1]);
+      }
+    }
+  }
+
+  return minutes;
 };
 
-const root = [1,5,3,null,4,10,6,9,2], start = 3
-console.log(amountOfTime(root, start))
+const root = new TreeNode(
+  1,
+  new TreeNode(5, null, new TreeNode(4, new TreeNode(9), new TreeNode(2))),
+  new TreeNode(3, new TreeNode(10), new TreeNode(6))
+);
+const start = 3;
+console.log(amountOfTime(root, start)); // Output: 4
