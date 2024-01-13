071e4876e3ef4b89d199f8d893f038b616c1d7d6
Felix <felix.zeeb@pm.me>
2024-01-13 14:34:51 +0100

    Minimum Number of Steps to Make Two Strings Anagram

--- minSteps.js
+++ minSteps.js
@@ -4,7 +4,22 @@
  * @return {number}
  */
 var minSteps = function(s, t) {
-    
+  
+      let count = 0;
+      let map = {};
+  
+      for (let i = 0; i < s.length; i++) {
+          map[s[i]] = map[s[i]] + 1 || 1;
+      }
+  
+      for (let i = 0; i < t.length; i++) {
+          if (map[t[i]]) {
+              map[t[i]]--;
+          } else {
+              count++;
+          }
+      }
+      return count;
 };
 
 const s = "bab", t = "aba"
