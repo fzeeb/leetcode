bcbd76285b861b714bdae84728a99d286b143846
Felix <felix.zeeb@pm.me>
2024-01-13 14:30:50 +0100

    inital commit

---
+++ minSteps.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {number}
+ */
+var minSteps = function(s, t) {
+    
+};
+
+const s = "bab", t = "aba"
+console.log(minSteps(s, t));  // 1
