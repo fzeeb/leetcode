411d0c35709087d3deffc7b9a85afa52f505dd26
Felix <felix.zeeb@pm.me>
2024-01-21 23:12:24 +0100

    make faster

--- sumSubarrayMins.js
+++ sumSubarrayMins.js
@@ -7,21 +7,19 @@ Given an array of integers arr, find the sum of min(b), where b ranges over ever
  */
 var sumSubarrayMins = function(arr) {
   let sum = 0;
-  const n = arr.length;
-  const dp = new Array(n).fill(0).map(() => new Array(n).fill(0));
-  const MOD = 1000000007; // Modulo value
-  for (let i = 0; i < n; i++) {
-    dp[0][i] = arr[i];
-    sum += arr[i];
-  }
-  for (let i = 1; i < n; i++) {
-    for (let j = 0; j < n - i; j++) {
-      dp[i][j] = Math.min(dp[i - 1][j], dp[i - 1][j + 1]);
-      sum += dp[i][j];
-      sum %= MOD; // Apply modulo
+  const MOD = 1000000007;
+  const stack = [];
+  arr = [0, ...arr, 0];
+  for (let i = 0; i < arr.length; i++) {
+    while (stack.length && arr[i] < arr[stack[stack.length - 1]]) {
+      const j = stack.pop();
+      const k = stack[stack.length - 1];
+      sum += arr[j] * (i - j) * (j - k);
+      sum %= MOD;
     }
+    stack.push(i);
   }
-  return sum;  
+  return sum; 
 };
 
 const arr = [3,1,2,4]
