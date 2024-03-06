8d02fdd2a6f2a2a37cd91ca3e8fc80bc0430bd50
Felix Zeeb <felix.zeeb@protonmail.com>
2024-03-06 11:31:35 +0100

    Linked List Cycle

---
+++ hasCycle.rb
@@ -0,0 +1,29 @@
+=begin
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+=end
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val)
+#         @val = val
+#         @next = nil
+#     end
+# end
+
+# @param {ListNode} head
+# @return {Boolean}
+def hasCycle(head)
+  return false if head.nil?
+  slow = head
+  fast = head.next
+  while slow != fast
+    return false if fast.nil? || fast.next.nil?
+    slow = slow.next
+    fast = fast.next.next
+  end
+  true
+end
