1a750ebcfc9b249e73e8099810cf1703a779e75f
Felix Zeeb <felix.zeeb@protonmail.com>
2024-03-25 21:54:33 +0100

    Find All Duplicates in an Array - js faster

--- findDuplicates.js
+++ findDuplicates.js
@@ -4,13 +4,18 @@ Given an integer array nums of length n where all the integers of nums are in th
 You must write an algorithm that runs in O(n) time and uses only constant extra space.
 */
 var findDuplicates = function(nums) {
-  let duplicates = []
-  nums = nums.sort()
-  for (let i = 1; i < nums.length; i++) {
-      if (nums[i-1] == nums[i]) {
-          duplicates.push(nums[i])
-          i++
-      }
+  let duplicates = [];
+  const n = nums.length;
+  
+  for (let i = 0; i < n; i++) {
+    const index = Math.abs(nums[i]) - 1;
+    
+    if (nums[index] < 0) {
+      duplicates.push(Math.abs(nums[i]));
+    } else {
+      nums[index] = -nums[index];
+    }
   }
-  return duplicates
+  
+  return duplicates;
 };
