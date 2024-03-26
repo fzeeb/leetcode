061a207c6a494edeafd6796b5df9c6a814a2b338
Felix Zeeb <felix.zeeb@protonmail.com>
2024-03-26 16:59:58 +0100

    First Missing Positive - js

---
+++ firstMissingPositive.js
@@ -0,0 +1,28 @@
+/*
+Given an unsorted integer array nums. Return the smallest positive integer that is not present in nums.
+
+You must implement an algorithm that runs in O(n) time and uses O(1) auxiliary space.
+*/
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function(nums) {
+    let n = nums.length;
+    for (let i = 0; i < n; i++) {
+        while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] !== nums[i]) {
+            let temp = nums[nums[i] - 1];
+            nums[nums[i] - 1] = nums[i];
+            nums[i] = temp;
+        }
+    }
+    for (let i = 0; i < n; i++) {
+        if (nums[i] !== i + 1) {
+            return i + 1;
+        }
+    }
+    return n + 1;
+};
+
+let nums = [1,2,0];
+console.log(firstMissingPositive(nums)); // 3
