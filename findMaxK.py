d31bf029e27c373327942527b1542b6f69dc36d6
Felix Zeeb <felix.zeeb@protonmail.com>
2024-05-02 10:48:40 +0200

    Make it faster using set

--- findMaxK.py
+++ findMaxK.py
@@ -40,6 +40,7 @@ Would a hash table help?
 """
 class Solution:
   def findMaxK(self, nums: List[int]) -> int:
+    nums = set(nums)
     max_k = -1
     for num in nums:
       if -num in nums and num > max_k:
