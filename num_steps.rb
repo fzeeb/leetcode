fda2f4e8a6affd31ea18a7f12e5bd82f68be2bd1
Felix Zeeb <felix.zeeb@protonmail.com>
2024-05-29 11:04:10 +0200

    Number of Steps to Reduce a Number in Binary Representation to One - rb

---
+++ num_steps.rb
@@ -0,0 +1,59 @@
+=begin
+Given the binary representation of an integer as a string s, return the number of steps to reduce it to 1 under the following rules:
+    If the current number is even, you have to divide it by 2.
+    If the current number is odd, you have to add 1 to it.
+It is guaranteed that you can always reach one for all test cases.
+
+Example 1:
+Input: s = "1101"
+Output: 6
+Explanation: "1101" corressponds to number 13 in their decimal representation.
+Step 1) 13 is odd, add 1 and obtain 14. 
+Step 2) 14 is even, divide by 2 and obtain 7.
+Step 3) 7 is odd, add 1 and obtain 8.
+Step 4) 8 is even, divide by 2 and obtain 4.  
+Step 5) 4 is even, divide by 2 and obtain 2. 
+Step 6) 2 is even, divide by 2 and obtain 1.  
+
+Example 2:
+Input: s = "10"
+Output: 1
+Explanation: "10" corressponds to number 2 in their decimal representation.
+Step 1) 2 is even, divide by 2 and obtain 1.  
+
+Example 3:
+Input: s = "1"
+Output: 0
+
+Constraints:
+    1 <= s.length <= 500
+    s consists of characters '0' or '1'
+    s[0] == '1'
+
+Hint 1
+Read the string from right to left, if the string ends in '0' then the number is even otherwise it is odd.
+
+Hint 2
+Simulate the steps described in the binary string.
+=end
+# @param {String} s
+# @return {Integer}
+def num_steps(s)
+  # Convert the binary string to integer
+  num = s.to_i(2)
+  steps = 0
+  while num > 1
+    if num.even?
+      num /= 2
+    else
+      num += 1
+    end
+    steps += 1
+  end
+  steps
+end
+
+
+puts num_steps("1101") # Expected 6
+puts  num_steps("10") # Expected 1
+puts  num_steps("1") # Expected 0
