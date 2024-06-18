824a19f60c7b793a9659665b96d5a42da0febfb4
Felix Zeeb <felix.zeeb@protonmail.com>
2024-06-18 15:26:25 +0200

    using a two-pointer technique to assign each worker the most profitable job

--- max_profit_assignment.rb
+++ max_profit_assignment.rb
@@ -27,27 +27,30 @@ Constraints:
 # @param {Integer[]} worker
 # @return {Integer}
 def max_profit_assignment(difficulty, profit, worker)
-  # get hash with difficulty as key and profit as value
-  jobs = Hash[difficulty.zip(profit)]
+  # Combine difficulty and profit into a list of jobs and sort them by difficulty
+  jobs = difficulty.zip(profit).sort_by { |d, p| d }
   worker.sort!
+
   max_profit = 0
-  # sort jobs hash by profit
-  jobs = jobs.sort_by { |k, v| -v }.to_h
-  # iterate through worker array
-  worker.each do |w|
-    # iterate through jobs hash
-    jobs.each do |d, p|
-      # if worker can do the job
-      if w >= d
-        # add profit to max_profit
-        max_profit += p
-        break
-      end
+  best_profit = 0
+  job_index = 0
+
+  # Iterate over each worker
+  worker.each do |ability|
+    # Update the best profit for jobs that the current worker can do
+    while job_index < jobs.size && jobs[job_index][0] <= ability
+      best_profit = [best_profit, jobs[job_index][1]].max
+      job_index += 1
     end
+    # Add the best profit the worker can achieve to the max_profit
+    max_profit += best_profit
   end
-  # return max profit
+
+  # Return the total maximum profit
   max_profit
 end
 
+# Test cases
 puts max_profit_assignment([2,4,6,8,10], [10,20,30,40,50], [4,5,6,7]) # 100
 puts max_profit_assignment([85,47,57], [24,66,99], [40,25,25]) # 0
+puts max_profit_assignment([66,1,28,73,53,35,45,60,100,44,59,94,27,88,7,18,83,18,72,63], [66,20,84,81,56,40,37,82,53,45,43,96,67,27,12,54,98,19,47,77], [61,33,68,38,63,45,1,10,53,23,66,70,14,51,94,18,28,78,100,16]) # 1392
