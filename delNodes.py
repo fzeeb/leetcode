8bafdfa691931f7ee2d370a325e476f9162e331e
Felix Zeeb <felix.zeeb@protonmail.com>
2024-07-17 21:41:43 +0200

    Delete Nodes And Return Forest

---
+++ delNodes.py
@@ -0,0 +1,56 @@
+"""
+Given the root of a binary tree, each node in the tree has a distinct value.
+After deleting all nodes with a value in to_delete, we are left with a forest (a disjoint union of trees).
+Return the roots of the trees in the remaining forest. You may return the result in any order.
+
+Example 1:
+Input: root = [1,2,3,4,5,6,7], to_delete = [3,5]
+Output: [[1,2,null,4],[6],[7]]
+
+Example 2:
+Input: root = [1,2,4,null,3], to_delete = [3]
+Output: [[1,2,4]]
+
+Constraints:
+    The number of nodes in the given tree is at most 1000.
+    Each node has a distinct value between 1 and 1000.
+    to_delete.length <= 1000
+    to_delete contains distinct values between 1 and 1000.
+"""
+from typing import List, Optional
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeNode]:
+        to_delete = set(to_delete)
+        res = []
+        def dfs(node, is_root):
+            if not node:
+                return None
+            is_deleted = node.val in to_delete
+            if is_root and not is_deleted:
+                res.append(node)
+            node.left = dfs(node.left, is_deleted)
+            node.right = dfs(node.right, is_deleted)
+            return None if is_deleted else node
+        dfs(root, True)
+        return res
+        
+# Main Call
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+root.right.left = TreeNode(6)
+root.right.right = TreeNode(7)
+
+to_delete = [3,5]
+solution = Solution()
+result = solution.delNodes(root, to_delete)
+for node in result:
+  print(node.val)
