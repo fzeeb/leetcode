dac19e3041f7ab2d93c49dc572e294e918885828
Felix Zeeb <felix.zeeb@protonmail.com>
2024-10-14 21:32:21 +0200

    Use heap to query for the maximum on O(log n) time

--- maxKelements.py
+++ maxKelements.py
@@ -1,14 +1,19 @@
 from math import ceil
 from typing import List
+import heapq
 
 class Solution:
     def maxKelements(self, nums: List[int], k: int) -> int:
+        max_heap = [-num for num in nums]
+        heapq.heapify(max_heap)
+        
         score = 0
-        l = len(nums) - 1
         while k > 0:
-            nums.sort()
-            score += nums[l]
-            nums[l] = ceil(nums[l] / 3)
+            max_value = -heapq.heappop(max_heap)
+            score += max_value
+
+            heapq.heappush(max_heap, -ceil(max_value / 3))
+
             k -= 1
         
         return score
